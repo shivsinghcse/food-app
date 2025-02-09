261a2973b8eddcd405abcb894e3c0a911d230d7d
Shiv Singh <shivsinghcse@gmail.com>
2025-02-09 22:54:45 +0530

    fixing cors issue

--- utils/constants.js
+++ utils/constants.js
@@ -11,7 +11,7 @@ export const MENU_API =
     'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8466937&lng=80.94616599999999&restaurantId=';
 
 export const RES_API =
-    'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&&page_type=DESKTOP_WEB_LISTING';
+    'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&page_type=DESKTOP_WEB_LISTING';
     
     ;
 
