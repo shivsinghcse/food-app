ae2497360cc8c758a9f84f3b1dc7dbc54856bfff
Shiv Singh <shivsinghcse@gmail.com>
2025-02-09 22:51:08 +0530

    fixing cors issue

--- utils/constants.js
+++ utils/constants.js
@@ -8,10 +8,12 @@ export const LOGO_URL =
     'https://img.freepik.com/free-vector/burger-cheese-with-fire-cartoon-vector-icon-illustration-food-object-icon-concept-isolated-premium_138676-5539.jpg?w=826&t=st=1695148160~exp=1695148760~hmac=15fbe29d7fb2f694e05ac95449f779d2228e6c1f46233dfd51c55f3bf5db4bdc';
 
 export const MENU_API =
-    'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8466937&lng=80.94616599999999&restaurantId=';
+    'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8466937&lng=80.94616599999999&restaurantId=';
 
 export const RES_API =
-    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+    'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&&page_type=DESKTOP_WEB_LISTING';
+    
+    ;
 
 
     // https://thingproxy.freeboard.io/fetch/
